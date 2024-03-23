78c2f5e29286d1cc3d162478f475a3c5fbf2d9c8
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-23 03:05:22 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomeLayout from "./pages/HomeLayout";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        {" "}
+        <h1>about page</h1>
+      </div>
+    ),
+  },
+]);
 const App = () => {
-  return <div>hey there</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ client/src/pages/HomeLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeLayout = () => {
+  return <div>home layout</div>;
+};
+
+export default HomeLayout;
