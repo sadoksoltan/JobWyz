5773ff8b0a64523b141c46a4ef1b56e7731e6725
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-24 03:00:37 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomeLayout from "./pages/HomeLayout";
+import AddJob from "./pages/AddJob";
+import Admin from "./pages/Admin";
+import AllJobs from "./pages/AllJobs";
+import DashboardLayout from "./pages/DashboardLayout";
+import DeleteJob from "./pages/DeleteJob";
+import EditJob from "./pages/EditJob";
+import Error from "./pages/Error";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import Stats from "./pages/Stats";
+
 const router = createBrowserRouter([
   {
     path: "/",
--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Stats = () => {
-  return <h1>stats pages</h1>;
+  return <h1>stats pages </h1>;
 };
 
 export default Stats;
