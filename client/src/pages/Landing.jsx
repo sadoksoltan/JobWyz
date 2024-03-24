969a3ac4f430693c74065556e2eee84a6e5ff52d
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-24 04:31:53 +0100

    Update Landing.jsx

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background: red;
-  color: white;
-`;
 
 const Landing = () => {
   return (
     <div>
       <h1>landing pages</h1>
-      <StyledBtn>btn</StyledBtn>
     </div>
   );
 };
