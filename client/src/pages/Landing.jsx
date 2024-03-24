e6c9e10a88ba20b84e50e732fcaacaa18e9a7ee9
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-24 04:39:43 +0100

    Update Landing.jsx

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Wrapper from "../assets/wrappers/LandingPage";
-
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
 const Landing = () => {
   return (
     <Wrapper>
