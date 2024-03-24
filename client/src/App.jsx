3bf27c636f12120b0222fc445459da4146b69c1f
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-24 03:55:14 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,10 @@ const router = createBrowserRouter([
     element: <HomeLayout />,
     children: [
       {
+        index: true,
+        element: <Landing />,
+      },
+      {
         path: "register",
         element: <Register />,
       },
