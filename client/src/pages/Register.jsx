526f240b70db2fb0d64c066b71c6d24435333afe
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-25 02:37:07 +0100

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 const Register = () => {
-  return <h1>Register pages</h1>;
-};
+  return (
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-input"
+            defaultValue="john"
+            required
+          />
+        </div>
 
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
 export default Register;
