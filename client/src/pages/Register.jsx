6ea61ec108c6e14945261a96debe7884b08f5f06
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-25 02:35:16 +0100

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 const Register = () => {
   return <h1>Register pages</h1>;
 };
