92ad1b12faaa55433d4cfe542eb28cce9dbb208e
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-25 02:55:11 +0100

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>login pages</h1>;
+  return (
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type="email" name="email" defaultValue="soltan@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="sadok1234" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
-
 export default Login;
