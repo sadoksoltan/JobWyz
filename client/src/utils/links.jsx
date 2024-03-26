bb103e6824216fb941ac74388f4d10da2bd4b686
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-26 02:36:50 +0100

    Create links.jsx

---
+++ client/src/utils/links.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { IoBarChartSharp } from "react-icons/io5";
+import { MdQueryStats } from "react-icons/md";
+import { FaWpforms } from "react-icons/fa";
+import { ImProfile } from "react-icons/im";
+import { MdAdminPanelSettings } from "react-icons/md";
