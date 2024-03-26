1950440378a724d493f90b26959862816ced17a7
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-26 01:43:11 +0100

    Update SmallSidebar.jsx

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { useDashboardContext } from "../pages/DashboardLayout";
 const SmallSidebar = () => {
+  const data = useDashboardContext();
+  console.log(data);
   return <Wrapper>Smallsidebar</Wrapper>;
 };
 
