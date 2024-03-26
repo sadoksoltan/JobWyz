9f2c3233e7c565a00370ddadb38b67c857a0fbd2
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-26 01:46:36 +0100

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaHome } from "react-icons/fa";
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  return (
+    <div>
+      <h2>navbar</h2>
+      <FaHome />
+    </div>
+  );
 };
-
 export default Navbar;
