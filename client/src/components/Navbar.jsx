737a940671e74ac2ebe4cb22f6ac1336f0d748a2
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-26 01:48:03 +0100

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,24 @@
-import React from "react";
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft } from "react-icons/fa";
+import Logo from "./Logo";
+
+import { useDashboardContext } from "../pages/DashboardLayout";
 const Navbar = () => {
+  const { toggleSidebar } = useDashboardContext();
   return (
-    <div>
-      <h2>navbar</h2>
-      <FaHome />
-    </div>
+    <Wrapper>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+        </div>
+        <div className="btn-container">toggle/logout</div>
+      </div>
+    </Wrapper>
   );
 };
+
 export default Navbar;
