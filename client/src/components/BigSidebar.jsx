9879486cd3726abd175363f77ebb6a89a844fe49
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-26 01:55:59 +0100

    Update BigSidebar.jsx

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useDashboardContext } from "../pages/DashboardLayout";
 const BigSidebar = () => {
   return <Wrapper>BigSidebar</Wrapper>;
 };
