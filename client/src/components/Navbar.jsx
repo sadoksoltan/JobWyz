a5a103a6a1c320af6088aefc13b1c86050caa61e
Sadok <130623297+sadoksoltan@users.noreply.github.com>
2024-03-27 02:17:19 +0100

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import Logo from "./Logo";
 
 import { useDashboardContext } from "../pages/DashboardLayout";
 import LogoutContainer from "./LogoutContainer";
+import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
   const { toggleSidebar } = useDashboardContext();
   return (
@@ -17,6 +18,7 @@ const Navbar = () => {
           <h4 className="logo-text">dashboard</h4>
         </div>
         <div className="btn-container">
+          <ThemeToggle />
           <LogoutContainer />
         </div>
       </div>
